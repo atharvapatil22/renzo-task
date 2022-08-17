dc2bfd695af46c1ae7ca7294175be4f7340a1ffc
atharvapatil22 <atharva.22.patil@gmail.com>
2022-08-17 08:53:45 +0530

    linked api, designed card ui

--- src/Components/Card/Card.jsx
+++ src/Components/Card/Card.jsx
@@ -1,8 +1,34 @@
 import React from "react";
 import styles from "./Card.module.css";
+import { GrUserWorker } from "react-icons/gr";
+import { FaUsers, FaCoins } from "react-icons/fa";
+import { MdPrecisionManufacturing } from "react-icons/md";
+import { VscTypeHierarchySub } from "react-icons/vsc";
 
 function Card({ data }) {
-  return <div className={styles.wrapper}>{data.name}</div>;
+  return (
+    <div className={styles.wrapper}>
+      <div className={styles.content}>
+        <h2>{data.name}</h2>
+
+        <p>
+          <MdPrecisionManufacturing /> Manufacturer: {data.manufacturer}
+        </p>
+        <p>
+          <VscTypeHierarchySub /> Class: {data.starship_class.toUpperCase()}
+        </p>
+        <p>
+          <FaCoins /> Cost: {data.cost_in_credits} credits
+        </p>
+        <p>
+          <GrUserWorker /> Crew: {data.crew}
+        </p>
+        <p>
+          <FaUsers /> Passengers: {data.passengers}
+        </p>
+      </div>
+    </div>
+  );
 }
 
 export default Card;
--- src/Screens/Home/Home.jsx
+++ src/Screens/Home/Home.jsx
@@ -1,31 +1,44 @@
-import React from "react";
+import axios from "axios";
+import React, { useEffect, useState } from "react";
 import Card from "../../Components/Card/Card";
 import styles from "./Home.module.css";
 
 function Home() {
+  const [list, setList] = useState([]);
+
+  useEffect(() => {
+    fetchData();
+  }, []);
+
+  const fetchData = () => {
+    const page = Math.floor(Math.random() * 3 + 1);
+
+    axios
+      .get(`https://swapi.dev/api/starships/?page=${page}`)
+      .then((res) => {
+        setList(res.data.results);
+      })
+      .catch((err) => {
+        console.log("Error: ", err);
+      });
+  };
+
   return (
     <div className={styles.wrapper}>
       <div className={styles.heading}>
-        <h2>People from the Star Wars universe</h2>
-        <button>refresh List</button>
+        <h2>Starships from a Galaxy Far Far Away</h2>
+        <button onClick={fetchData}>refresh List</button>
       </div>
 
-      <div className={styles.list}>
-        {peopleList.map((item) => (
-          <Card data={item} key={item.id} />
-        ))}
+      <div className={styles.grid_container}>
+        <div>
+          {list.map((item, index) => (
+            <Card data={item} key={index} />
+          ))}
+        </div>
       </div>
     </div>
   );
 }
 
 export default Home;
-
-const peopleList = [
-  { id: 1, name: "abcd" },
-  { id: 2, name: "idsa" },
-  { id: 3, name: "vhi" },
-  { id: 4, name: "bois" },
-  { id: 5, name: "fodsai" },
-  { id: 6, name: "ioas" },
-];
